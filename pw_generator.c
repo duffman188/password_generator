b65aeeba4dd85734a8152c7c752741113e92c2c0
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2025-04-06 15:21:54 -0600

    Create pw_generator.c

---
+++ pw_generator.c
@@ -0,0 +1,10 @@
+// pw_generator.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "pw_generator.h"
+
+// Function to generate a password
+void generate_password(char *password, int length, const char *available_chars) {
+    // TODO: Implement password generation logic
+}
