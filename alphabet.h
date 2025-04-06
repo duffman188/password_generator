c8b20a850ff6ee41592f02f3d49a7e7947db001d
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2025-04-06 15:20:58 -0600

    Update alphabet.h

--- alphabet.h
+++ alphabet.h
@@ -1,7 +1,14 @@
+// alphabet.h
+
 #ifndef ALPHABET_H
 #define ALPHABET_H
 
+#define MAX_ALPHABET_SIZE 128
+
+// Function to construct the available character set from flags and custom alphabet
 void get_alphabet_union(const char *flags, const char *user_alphabet, char *available_chars);
+
+// Function to validate the user-provided custom alphabet
 void validate_alphabet(const char *alphabet);
 
-#endif
+#endif // ALPHABET_H
