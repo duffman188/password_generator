8c96618ab9081f13599d76d7fc6a0f9d16e9d519
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2025-04-06 15:22:06 -0600

    Update information_content.h

--- information_content.h
+++ information_content.h
@@ -1,6 +1,9 @@
+// information_content.h
+
 #ifndef INFORMATION_CONTENT_H
 #define INFORMATION_CONTENT_H
 
+// Function to calculate the information content (entropy) of a password
 double calculate_information_content(const char *password, const char *available_chars);
 
-#endif
+#endif // INFORMATION_CONTENT_H
