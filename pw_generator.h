b999f3bdb8775a8aeb99a4abdbcd6ad052d0e565
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2025-04-06 15:21:36 -0600

    Update pw_generator.h

--- pw_generator.h
+++ pw_generator.h
@@ -1,6 +1,9 @@
+// pw_generator.h
+
 #ifndef PW_GENERATOR_H
 #define PW_GENERATOR_H
 
+// Function to generate a random password from the available characters
 void generate_password(char *password, int length, const char *available_chars);
 
-#endif
+#endif // PW_GENERATOR_H
