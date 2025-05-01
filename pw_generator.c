ed1af6050e13a9e88b8ae6c775cff3db25cde82e
duffman188 <laron.latin@yahoo.com>
2025-05-01 10:04:08 -0600

    latest

--- pw_generator.c
+++ pw_generator.c
@@ -1,10 +1,21 @@
 // pw_generator.c
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "pw_generator.h"
 
-// Function to generate a password
 void generate_password(char *password, int length, const char *available_chars) {
-    // TODO: Implement password generation logic
+    size_t alphabet_size = strlen(available_chars);
+
+    if (alphabet_size == 0) {
+        password[0] = '\0';  // fail-safe
+        return;
+    }
+
+    for (int i = 0; i < length; i++) {
+        int index = rand() % alphabet_size;
+        password[i] = available_chars[index];
+    }
+
+    password[length] = '\0';  // null-terminate
 }
